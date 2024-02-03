11510006945358abe6ac3749a5efaaaf6ce54187
Abzal <abzaltaaabzal@gmail.com>
2024-02-03 17:00:35 +0600

    sdv

---
+++ lab03/classes.py
@@ -0,0 +1,12 @@
+class MyClass:
+    x = 5
+
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+class MyClass:
+    x = 5
+p1 = MyClass()
+print(p1.x)
+
---
+++ lab03/modules.py
@@ -0,0 +1,8 @@
+import mymodule
+
+import mymodule as mx
+
+import mymodule
+print(dir(mymodule))
+
+from mymodule import person1
