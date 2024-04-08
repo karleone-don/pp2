38d367831a3a5825736144ec0932add66d5407a6
Abzal <abzaltaaabzal@gmail.com>
2024-04-08 19:02:46 +0500

    sdf

--- lab8and9/paint.py
+++ lab8and9/paint.py
@@ -3,10 +3,12 @@ import sys
 
 pygame.init()
 
+# Устанавливаем размеры окна и панели
 height = 600
 panel_height = 100
 width = 800
 
+# Создаем окно и поверхности
 window = pygame.display.set_mode((width, height))
 screen = pygame.Surface((width, height - panel_height))
 another_layer = pygame.Surface((width, height - panel_height))
@@ -14,6 +16,7 @@ panel = pygame.Surface((width, panel_height))
 
 queue = []
 
+# Функции для создания различных фигур
 def getRectangle(x1, y1, x2, y2):
         x = min(x1, x2)
         y = min(y1, y2)
@@ -21,15 +24,25 @@ def getRectangle(x1, y1, x2, y2):
         h = abs(y1-y2)
         return (x, y, w, h)
 
-def getCircle(x1, y1, x2, y2):
-    x = min(x1, x2)
-    y = min(y1, y2)
-    return (x, y)
+def getSquare(x1, y1, x2, y2):
+    w = abs(x1-x2)
+    h = abs(y1-y2)
+    w = min(w, h)
+    if x1 < x2:
+        x = x1
+    else:
+        x = x1 - w
+    if y1 < y2:
+        y = y1
+    else:
+        y = y1 - w
+    return(x, y, w, w)
 
 def getRadius(x1, y1, x2, y2):
     r = max(abs(x1-x2), abs(y1-y2))
     return r
 
+# Функция для заполнения области
 def step(screen, x, y, origin_color, fill_color):
     if x < 0 or y < 0: return False
     if x > width-1 or y > height-panel_height-1: return False
@@ -37,7 +50,8 @@ def step(screen, x, y, origin_color, fill_color):
     queue.append((x, y))
     screen.set_at((x, y), fill_color)
 
-pencil = pygame.image.load('lab8/img/pencil.png')
+# Загружаем изображения инструментов
+pencil = pygame.image.load('lab8and9/img/pencil.png')
 pencil = pygame.transform.scale(pencil, (75, 75))
 rect = pencil.get_rect()
 rect1 = rect.move(10, 10)
@@ -49,24 +63,19 @@ rect6 = rect.move(435, 10)
 rect7 = rect.move(520, 10)
 rect8 = rect.move(605, 10)
 
-bucket = pygame.image.load('lab8/img/bucket.png')
+bucket = pygame.image.load('lab8and9/img/bucket.png')
 bucket = pygame.transform.scale(bucket, (75, 75))
 
-eraser = pygame.image.load('lab8/img/eraser.png')
+eraser = pygame.image.load('lab8and9/img/eraser.png')
 eraser = pygame.transform.scale(eraser, (75, 75))
 
-figures = pygame.image.load('lab8/img/figures.png')
+figures = pygame.image.load('lab8and9/img/figures.png')
 figures = pygame.transform.scale(figures, (75, 75))
 
-rectangle = pygame.image.load('lab8/img/rectangle.png')
-rectangle = pygame.transform.scale(rectangle, (75, 95))
-
-circle = pygame.image.load('lab8/img/circle.png')
-circle = pygame.transform.scale(circle, (75, 75))
-
-pallete = pygame.image.load('lab8/img/palette.png')
+pallete = pygame.image.load('lab8and9/img/palette.png')
 pallete = pygame.transform.scale(pallete, (75, 75))
 
+# Устанавливаем цвета
 BLACK = (0, 0, 0)
 DARK_GRAY = (50, 50, 50)
 BLUE = (0, 0, 255)
@@ -92,15 +101,31 @@ poligons = False
 colors = False
 
 while True:
+    # Рисуем панель
     panel.fill(DARK_GRAY)
     if poligons:
-        panel.blit(rectangle, (10, 10))
-        panel.blit(circle, (95, 10))
+        # Рисуем кнопки для выбора фигур
+        pygame.draw.rect(panel, WHITE, (15, 22, 65, 52), 4, border_radius=3)
+        pygame.draw.circle(panel, WHITE, (133, 48), 30, 5)
+        pygame.draw.rect(panel, WHITE, (190, 20, 55, 55), 4, border_radius=3)
+        pygame.draw.polygon(panel, WHITE, [[280, 75], [300, 15], [320, 75]], 5)
+        pygame.draw.polygon(panel, WHITE, [[360, 75], [387, 15], [415, 75]], 5)
+        pygame.draw.polygon(panel, WHITE, [[439, 47], [472, 14], [505, 47], [472, 80]], 5)
+        # Отображаем выделение выбранного инструмента
         if tool == 1:
             pygame.draw.rect(panel, BLUE, rect1, 1)
-        if tool == 4:
+        elif tool == 4:
             pygame.draw.rect(panel, BLUE, rect2, 1)
+        elif tool == 5:
+            pygame.draw.rect(panel, BLUE, rect3, 1)
+        elif tool == 6:
+            pygame.draw.rect(panel, BLUE, rect4, 1)
+        elif tool == 7:
+            pygame.draw.rect(panel, BLUE, rect5, 1)
+        elif tool == 8:
+            pygame.draw.rect(panel, BLUE, rect6, 1)
     elif colors:
+        # Рисуем палитру цветов
         pygame.draw.rect(panel, BLUE, (15, 15, 65, 65), border_radius=10)
         pygame.draw.rect(panel, WHITE, (100, 15, 65, 65), border_radius=10)
         pygame.draw.rect(panel, RED, (185, 15, 65, 65), border_radius=10)
@@ -109,6 +134,7 @@ while True:
         pygame.draw.rect(panel, ORANGE, (440, 15, 65, 65), border_radius=10)
         pygame.draw.rect(panel, VIOLETE, (525, 15, 65, 65), border_radius=10)
         pygame.draw.rect(panel, BLACK, (610, 15, 65, 65), border_radius=10)
+        # Отображаем выделение выбранного цвета
         if COLOR == BLUE:
             pygame.draw.rect(panel, BLUE, rect1, 1)
         elif COLOR == WHITE:
@@ -126,11 +152,13 @@ while True:
         elif COLOR == BLACK:
             pygame.draw.rect(panel, BLUE, rect8, 1)
     else:
+        # Отображаем инструменты рисования
         panel.blit(pencil, (10, 10))
         panel.blit(bucket, (95, 10))
         panel.blit(eraser, (180, 10))
         panel.blit(figures, (265, 10))
         panel.blit(pallete, (605, 10))
+        # Отображаем выделение выбранного инструмента
         if tool == 0:
             pygame.draw.rect(panel, BLUE, rect1, 1)
         elif tool == 2:
@@ -172,11 +200,21 @@ while True:
                 mouse_pressed = True
                 if pos[1] < 100:
                     if poligons:
+                        #смена фигуры
                         if 10 < pos[0] and pos[0] < 90:
                             tool = 1
                         elif 95 < pos[0] and pos[0] < 175:
                             tool = 4
+                        elif 180 < pos[0] and pos[0] < 260:
+                            tool = 5
+                        elif 265 < pos[0] and pos[0] < 345:
+                            tool = 6
+                        elif 350 < pos[0] and pos[0] < 430:
+                            tool = 7
+                        elif 435 < pos[0] and pos[0] < 515:
+                            tool = 8
                     elif colors:
+                        #смена цвета
                         if 10 < pos[0] and pos[0] < 90:
                             COLOR = BLUE
                         elif 95 < pos[0] and pos[0] < 175:
@@ -194,6 +232,7 @@ while True:
                         elif 615 < pos[0] and pos[0] < 695:
                             COLOR = BLACK
                     else:
+                        #смена инстрмента
                         if 10 < pos[0] and pos[0] < 90:
                             tool = 0
                         elif 95 < pos[0] and pos[0] < 175:
@@ -215,28 +254,42 @@ while True:
         if e.type == pygame.MOUSEMOTION:
             if mouse_pressed:
                 if tool == 0:
+                    #карандаш
                     x1 = x2
                     y1 = y2
                     x2 = pos[0]
                     y2 = pos[1] - panel_height
                     pygame.draw.line(screen, COLOR, (x1, y1), (x2, y2))
-                elif tool == 1:
-                    screen.blit(another_layer, (0, 0))
+                else:
                     x2 = pos[0]
                     y2 = pos[1] - panel_height
-                    pygame.draw.rect(screen, COLOR, pygame.Rect(getRectangle(x1, y1, x2, y2)), 1)
-                elif tool == 4:
-                    screen.blit(another_layer, (0, 0))
-                    x2 = pos[0]
-                    y2 = pos[1] - panel_height
-                    pygame.draw.circle(screen, COLOR,  (getCircle(x1, y1, x2, y2)), getRadius(x1, y1, x2, y2), 2)
-                elif tool == 3:
-                    x2 = pos[0]
-                    y2 = pos[1] - 100
-                    pygame.draw.rect(screen, BLACK, (x2-25, y2-25, 50, 50), border_radius=10)
+                    if tool == 3:
+                        #стерка
+                        pygame.draw.rect(screen, BLACK, (x2-25, y2-25, 50, 50), border_radius=10)
+                    else:
+                        screen.blit(another_layer, (0, 0))
+                        if tool == 1:
+                            #рисование фигур
+                            pygame.draw.rect(screen, COLOR, pygame.Rect(getRectangle(x1, y1, x2, y2)), 1)
+                        elif tool == 4:
+                            pygame.draw.circle(screen, COLOR,  (x1, y1), getRadius(x1, y1, x2, y2), 2)
+                        elif tool == 5:
+                            pygame.draw.rect(screen, COLOR, pygame.Rect(getSquare(x1, y1, x2, y2)), 1)
+                        elif tool == 6:
+                            pygame.draw.polygon(screen, COLOR, [[x1, y2], [(x1+x2)/2, y1], [x2, y2]], 2)
+                        elif tool == 7:
+                            pygame.draw.polygon(screen, COLOR, [[x1, y1], 
+                                                                [(x1+x2)/2, y1 - abs(x2-x1)*0.87], 
+                                                                [x2, y1]], 2)
+                        elif tool == 8:
+                            pygame.draw.polygon(screen, COLOR, [[x1, (y1+y2)/2], 
+                                                                [(x1+x2)/2, y1],
+                                                                [x2, (y1+y2)/2], 
+                                                                [(x1+x2)/2, y2] 
+                                                                ], 2)
         
    
-
+    #обновление экрана
     window.blit(panel, (0, 0))
     window.blit(screen, (0, 100))
     pygame.display.update()
